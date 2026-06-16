13b1b33f56c5c31f054b42232df601689e00ce44
Ramadhan MF <9th.ramadhan@gmail.com>
2026-06-16 18:33:57 +0800

    mengganti navigas dashboardForm user ke dashboardForm Baru

--- src/gui/DashboardFormNew.java
+++ src/gui/DashboardFormNew.java
@@ -324,7 +324,7 @@ public class DashboardFormNew extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButtonHitungTarifActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonHitungTarifActionPerformed
-        HitungTarifForm h = new HitungTarifForm(penumpang);
+        HitungTarifFormNew h = new HitungTarifFormNew(penumpang);
         h.setVisible(true);
         this.dispose();        // TODO add your handling code here:
     }//GEN-LAST:event_jButtonHitungTarifActionPerformed
@@ -337,32 +337,6 @@ public class DashboardFormNew extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-//    public static void main(String args[]) {
-//        /* Set the Nimbus look and feel */
-//        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-//        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-//         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-//         */
-//        try {
-//            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-//                if ("Nimbus".equals(info.getName())) {
-//                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-//                    break;
-//                }
-//            }
-//        } catch (ReflectiveOperationException | javax.swing.UnsupportedLookAndFeelException ex) {
-//            logger.log(java.util.logging.Level.SEVERE, null, ex);
-//        }
-//        //</editor-fold>
-//
-//        /* Create and display the form */
-//        java.awt.EventQueue.invokeLater(() -> {
-//            // Membuat objek penumpang dummy/kosong agar constructor tidak error
-//            model.Penumpang penumpangDummy = new model.Penumpang();
-//            new DashboardFormNew(penumpangDummy).setVisible(true);
-//        });
-//    }
-
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButtonHitungTarif;
